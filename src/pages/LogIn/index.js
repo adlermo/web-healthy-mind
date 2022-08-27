366c70a8c8b9d896faec0dac959043f12209b453
Luciano Ilha <ilha.dev.br@gmail.com>
2022-08-26 21:52:55 -0300

    Finish login layout.

--- src/pages/LogIn/index.js
+++ src/pages/LogIn/index.js
@@ -32,70 +32,74 @@ const LogIn = () => {
           />
         </Sider>
         <Layout>
-          <Form
-            className='login-box'
-            name="basic"
-            labelCol={{
-              span: 8,
-            }}
-            wrapperCol={{
-              span: 16,
-            }}
-            initialValues={{
-              remember: true,
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-          >
-            <Form.Item
-              label="Email"
-              name="email"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your best email!',
-                },
-              ]}
-            >
-              <Input className='login-item' />
-            </Form.Item>
-
-            <Form.Item
-              label="Password"
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your password!',
-                },
-              ]}
-            >
-              <Input.Password className='login-item' />
-            </Form.Item>
-
-            <Form.Item
-              name="remember"
-              valuePropName="checked"
+          <div className='form-box'>
+            <Form
+              className='login-box'
+              name="basic"
+              labelCol={{
+                span: 8,
+              }}
               wrapperCol={{
-                offset: 8,
                 span: 16,
               }}
-            >
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-
-            <Form.Item
-              wrapperCol={{
-                offset: 8,
-                span: 16,
+              initialValues={{
+                remember: true,
               }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              autoComplete="off"
             >
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
+              <h1 className='login-title'>Bem vindo ao sistema</h1>
+              <div className='login-subtitle'>Por favor entre com a sua conta</div>
+              <Form.Item
+                label="Email"
+                name="email"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your best email!',
+                  },
+                ]}
+              >
+                <Input className='login-item' />
+              </Form.Item>
+
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your password!',
+                  },
+                ]}
+              >
+                <Input.Password className='login-item' />
+              </Form.Item>
+
+              <Form.Item
+                name="remember"
+                valuePropName="checked"
+                wrapperCol={{
+                  offset: 8,
+                  span: 16,
+                }}
+              >
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+              <div className='login-subtitle subtitle2'>Criar conta</div>
+              <Form.Item
+                wrapperCol={{
+                  offset: 8,
+                  span: 16,
+                }}
+              >
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
           <Footer
             style={{
               textAlign: 'center',
