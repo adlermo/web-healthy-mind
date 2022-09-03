036a9f8db801173ba2b3211d6dffdb4df7d6db3f
Luciano Ilha <ilha.dev.br@gmail.com>
2022-09-03 09:58:36 -0300

    Add patients and sessions table component.

--- src/components/PatientSessionList/PatientSessionList.tsx
+++ src/components/PatientSessionList/PatientSessionList.tsx
@@ -1,14 +1,87 @@
 import React from 'react'
 import SideMenu from '../SideMenu/SideMenu';
-import { Layout } from 'antd';
+import { Layout, Typography, Input, Button, Table } from 'antd';
+import { PlusCircleOutlined } from '@ant-design/icons';
+import { MainBox, UpperBox, BottomBox } from './PatientSessionListStyles';
 
 const PatientSessionList: React.FC = () => {
     const { Footer } = Layout;
+    const { Title } = Typography;
+    const { Search } = Input;
+
+    const dataSource = [
+        {
+            key: '1',
+            name: 'Mike',
+            age: 32,
+            address: '10 Downing Street',
+        },
+        {
+            key: '2',
+            name: 'John',
+            age: 42,
+            address: '10 Downing Street',
+        },
+    ];
+    
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Endereço',
+            dataIndex: 'address',
+            key: 'Address',
+        },
+        {
+            title: 'Telefone',
+            dataIndex: 'phone',
+            key: 'phone',
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            key: 'email',
+        },
+        {
+            title: 'Criado em:',
+            dataIndex: 'createdAt',
+            key: 'createdAt',
+        },
+    ];
+
+    const onSearch = (value: string) => console.log(value);
 
     return(
         <Layout>
             <SideMenu />
             <Layout>
+                <MainBox>
+                    <UpperBox>
+                        <Title level={3}>Meus Pacientes</Title>
+                        <Search
+                            placeholder="input search text"
+                            onSearch={onSearch} enterButton
+                            style={{
+                                width: `45%`,
+                            }}
+                        />
+                        <Button type="primary" href='/register-patient' icon={<PlusCircleOutlined />}>
+                            Novo paciente
+                        </Button>
+                    </UpperBox>
+                    <BottomBox>
+                        <Table
+                            dataSource={dataSource}
+                            columns={columns}
+                            style={{
+                                width: `100%`,
+                            }}
+                        />
+                    </BottomBox>
+                </MainBox>
                 <Footer
                     style={{
                         textAlign: 'center',
