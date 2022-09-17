183ff2d330e2ab4e1842d5a8d061ab27b7f2d4a5
adlermo <adlerorteney@gmail.com>
2022-09-16 21:42:33 -0300

    Full height to PatientRegisterForm

--- src/components/FormPatient/FormPatient.tsx
+++ src/components/FormPatient/FormPatient.tsx
@@ -6,6 +6,7 @@ import type { DatePickerProps } from 'antd';
 import { Button, DatePicker, Form, Input, message, Layout, InputNumber } from 'antd';
 import { useMutation } from '@tanstack/react-query';
 import { fetchRegisterPatient } from 'src/services/Patient/service';
+import { Content } from 'antd/lib/layout/layout';
 import SideMenu from '../SideMenu/SideMenu';
 import { Welcome } from './FormPatientStyles';
 
@@ -70,122 +71,125 @@ const FormPatient: React.FC = () => {
   };
 
   return (
-    <Layout>
+    <Layout style={{ minHeight: '100vh' }}>
       <SideMenu />
       <Layout>
-        <Form
-          name="basic"
-          labelCol={{
-            span: 6,
-          }}
-          wrapperCol={{
-            span: 12,
-          }}
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off">
-          <Form.Item
+        <Content>
+          <Form
+            name="basic"
+            labelCol={{
+              span: 6,
+            }}
             wrapperCol={{
-              offset: 6,
               span: 12,
-            }}>
-            <Welcome>Cadastro do Paciente</Welcome>
-          </Form.Item>
-
-          <Form.Item
-            label="Nome"
-            name="name"
-            rules={[
-              {
-                required: true,
-                message: 'Nome do paciente',
-              },
-            ]}>
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[
-              {
-                required: true,
-                message: 'Email do paciente',
-              },
-            ]}>
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="Documento"
-            name="document"
-            rules={[
-              {
-                required: true,
-                message: 'Documento do paciente',
-              },
-            ]}>
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="Gênero"
-            name="gender"
-            rules={[
-              {
-                required: true,
-                message: 'Gênero do paciente',
-              },
-            ]}>
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="Data de nascimento"
-            name="birthDate"
-            rules={[
-              {
-                required: true,
-                message: 'Data de nascimento do paciente',
-              },
-            ]}>
-            <DatePicker onChange={onChange} />
-          </Form.Item>
-
-          <Form.Item
-            label="Telefone"
-            name="phone"
-            rules={[
-              {
-                required: true,
-                message: 'Telefone do paciente',
-              },
-            ]}>
-            <InputNumber style={{ width: 170 }} />
-          </Form.Item>
-
-          <Form.Item
-            wrapperCol={{
-              offset: 6,
-              span: 12,
-            }}>
-            <Button
-              type="default"
-              href="/patients"
-              style={{
-                marginRight: 30,
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off">
+            <Form.Item
+              wrapperCol={{
+                offset: 6,
+                span: 12,
               }}>
-              Cancelar
-            </Button>
+              <Welcome>Cadastro do Paciente</Welcome>
+            </Form.Item>
 
-            <Button type="primary" htmlType="submit">
-              Salvar
-            </Button>
-          </Form.Item>
-        </Form>
+            <Form.Item
+              label="Nome"
+              name="name"
+              rules={[
+                {
+                  required: true,
+                  message: 'Nome do paciente',
+                },
+              ]}>
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: 'Email do paciente',
+                },
+              ]}>
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Documento"
+              name="document"
+              rules={[
+                {
+                  required: true,
+                  message: 'Documento do paciente',
+                },
+              ]}>
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Gênero"
+              name="gender"
+              rules={[
+                {
+                  required: true,
+                  message: 'Gênero do paciente',
+                },
+              ]}>
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Data de nascimento"
+              name="birthDate"
+              rules={[
+                {
+                  required: true,
+                  message: 'Data de nascimento do paciente',
+                },
+              ]}>
+              <DatePicker onChange={onChange} />
+            </Form.Item>
+
+            <Form.Item
+              label="Telefone"
+              name="phone"
+              rules={[
+                {
+                  required: true,
+                  message: 'Telefone do paciente',
+                },
+              ]}>
+              <InputNumber style={{ width: 170 }} />
+            </Form.Item>
+
+            <Form.Item
+              wrapperCol={{
+                offset: 6,
+                span: 12,
+              }}>
+              <Button
+                type="default"
+                href="/patients"
+                style={{
+                  marginRight: 30,
+                }}>
+                Cancelar
+              </Button>
+
+              <Button type="primary" htmlType="submit">
+                Salvar
+              </Button>
+            </Form.Item>
+          </Form>
+        </Content>
+
         <Footer
           style={{
             textAlign: 'center',
