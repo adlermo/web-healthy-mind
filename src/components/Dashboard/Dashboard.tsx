34f8cf08c392ee6cca70fcbd7cb1748b3f172d7c
Luciano Ilha <ilha.dev.br@gmail.com>
2022-09-17 12:25:27 -0300

    Add first patient grid layout

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,7 +1,14 @@
-import { Col, Layout, Row } from 'antd';
+import { Col, Layout, Row, Divider, Descriptions, Badge } from 'antd';
 import { getUserRole } from 'src/services/Auth/service';
 import SideMenu from '../SideMenu/SideMenu';
-import { GridItem, ItemNumber, ItemTitle, LayoutBackground } from './DashboardStyles';
+import {
+  GridItem,
+  PatientGridItem,
+  ItemNumber,
+  ItemTitle,
+  LayoutBackground,
+  PatientItemTitle,
+} from './DashboardStyles';
 
 const Dashboard: React.FC = () => {
   const currentPath = window.location.pathname;
@@ -16,6 +23,32 @@ const Dashboard: React.FC = () => {
           style={{
             margin: '25px 20px',
           }}>
+          <LayoutBackground>
+            <Row gutter={[0, 40]} wrap>
+              <Col span={24}>
+                <PatientGridItem>
+                  <PatientItemTitle>Titulo da sessao</PatientItemTitle>
+                  <Divider />
+                  <Descriptions layout="vertical" bordered>
+                    <Descriptions.Item label="Status" span={3}>
+                      <Badge status="processing" text="Running" />
+                    </Descriptions.Item>
+                  </Descriptions>
+                </PatientGridItem>
+              </Col>
+              <Col span={24}>
+                <PatientGridItem>
+                  <PatientItemTitle>Titulo da sessao</PatientItemTitle>
+                  <Divider />
+                  <Descriptions layout="vertical" bordered>
+                    <Descriptions.Item label="Status" span={3}>
+                      <Badge status="processing" text="Running" />
+                    </Descriptions.Item>
+                  </Descriptions>
+                </PatientGridItem>
+              </Col>
+            </Row>
+          </LayoutBackground>
           {userRole !== 'patient' && (
             <LayoutBackground>
               <Row gutter={[20, 20]} wrap>
